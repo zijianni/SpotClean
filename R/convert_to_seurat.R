05708aca5d53c429627d5e0500ee2ea97dacec04
Zijian Ni <412182180@qq.com>
2022-04-29 22:38:41 -0500

    v0.99.7 added more details to ConvertToSeurat()

--- R/convert_to_seurat.R
+++ R/convert_to_seurat.R
@@ -12,8 +12,11 @@
 #' should include files \code{tissue_lowres_iamge.png},
 #' \code{scalefactors_json.json} and \code{tissue_positions_list.csv}.
 #'
-#' @param filter_matrix (logical) If TRUE, only keep spots that have been
-#' determined to be over tissue. Default: \code{TRUE}
+#' @param filter_matrix (logical) If \code{TRUE}, only keep spots that have been
+#' determined to be over tissue. If \code{slide_obj} only contains tissue spots,
+#' \code{filter_matrix} has to be set \code{TRUE}. If \code{slide_obj}
+#' contains both tissue and background spots, setting \code{filter_matrix=TRUE}
+#' will subset the expression matrix to tissue spots only. Default: \code{TRUE}
 #'
 #' @param slice (chr) Name for the stored image of the tissue slice.
 #' Default: "slice1"
@@ -33,6 +36,7 @@
 #' }
 #'
 #' @importFrom Seurat Read10X_Image Cells CreateSeuratObject DefaultAssay<-
+#' @importFrom Seurat GetTissueCoordinates
 #' @importFrom SummarizedExperiment assay
 #' @importFrom S4Vectors metadata
 #' @export
@@ -48,6 +52,16 @@ ConvertToSeurat <- function(slide_obj, image_dir,
     # load image and add to Seurat object
     image <- Read10X_Image(image.dir = image_dir,
                            filter.matrix = filter_matrix)
+    ts_coord <- GetTissueCoordinates(image)
+
+    if(nrow(ts_coord)>ncol(object)){
+        stop("The slide object has fewer spots than the image data.
+Consider setting filter_matrix=TRUE?")
+    }
+    if(nrow(ts_coord)<ncol(object)){
+        object <- object[,rownames(ts_coord)]
+    }
+
     image <- image[Cells(x = object)]
     DefaultAssay(object = image) <- "Spatial"
     object[[slice]] <- image
